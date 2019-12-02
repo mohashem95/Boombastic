7d7010399e52d41e6ff5a5aec96a4f919d5c33a2
yazeedth <yazeedth7@gmail.com>
2019-12-02 04:06:55 +0300

    fake data

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask ,  render_template, json, url_for, request, session
 from flask_mail import Mail , Message
-from waitress import serve
-import os
+#from waitress import serve
+#import os
 
 app = Flask(__name__)
 
@@ -151,11 +151,14 @@ def meal_name(meals, m_name):
         elif(meals[i]['name'] == 'Margherita' and m_name == 'Margherita'):
             yield meals[i]
             json.dumps(meals[i])
+
+        print("-- DEBUG MODE ----")
+        app.run(debug=True, port='5555')
             
-if __name__ == '__main__':
-     print("--PRODUCTION MODE ---")
-     p = os.environ.get('PORT')
-     serve(app, host='0.0.0.0', port=p)
+#if __name__ == '__main__':
+     #print("--PRODUCTION MODE ---")
+     #p = os.environ.get('PORT')
+     #serve(app, host='0.0.0.0', port=p)
 
 
 
